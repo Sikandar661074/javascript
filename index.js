77f723019f82eaa53cd65a0e662d5b193641d3f5
sikandar661074 <sikandar661074@gmail.com>
2020-02-29 20:05:13 +0530

    my first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+function changename(){
+    document.getElementById("name").innerHTML="HEY THERE";
+    document.getElementById("name").style.cssText="color:red; font-size:40px; padding:10px; background:black;";
+    document.getElementById("btn").style.cssText="display:block; margin:0 auto; padding:10px;  background:yellow; font-size:20px; font-weight:700; border:0.5px solid yellow;border-radius:25px;";
+    document.getElementById("body").style.backgroundColor="gray";
+    document.getElementById("btn").innerHTML="done";
+
+}
