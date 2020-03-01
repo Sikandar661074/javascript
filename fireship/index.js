245a0768137005a21f87942edfe302193bf1acd8
sikandar661074 <sikandar661074@gmail.com>
2020-03-01 08:55:26 +0530

    practiced

---
+++ fireship/index.js
@@ -0,0 +1,106 @@
+// console.log(typeof (23));
+// x = {};
+// x['foo'] = '13'
+// console.log(x);
+// var bar = String('hello')
+// var foo = true
+// console.log(bar);
+
+// console.log(typeof (foo));
+
+// // truthy and falsy
+
+// console.log(!! '');
+// console.log(!! 'hey');
+// console.log(!! 0);
+// console.log(!! 1);
+
+// var x = 10;
+// if(x==10){
+//     x = 1
+// }
+// else{
+//     x=2
+// }
+// console.log(x);
+
+
+//   var expression = 40;
+//   switch(expression){
+//       case (expression < 50) :
+//           console.log('fail');
+//           break;
+//       case (expression=50) :
+//           console.log('pass');
+//           break;
+//  }
+
+// var i=0;
+// while(i<5){
+//     console.log(i++);
+    
+// }
+
+// var n=5;
+// for(i=0;i<n;i++){
+//     console.log(i);
+    
+// }
+
+result="";
+var i=0;
+do{
+    i=i+1;
+    result=result+i;
+}while(i<5);
+console.log(result);
+
+    
+
+
+// // try and catch
+// try{
+//     console.log(x);
+// } catch (error){
+//     console.log('broke');
+// } finally {
+//     console.log('always works');
+    
+// }
+
+// var g = 'global' ;
+// function app(){
+//     console.log(g);
+    
+//     l = 'local' ;
+//     console.log(l);
+// }
+// app()
+
+// var a = 10;
+// var b = 20;
+// var c = a*b;
+
+// function multiply(){
+//     console.log(c);
+// }
+// multiply()
+
+ function add(x,y){
+     var c= x + y;
+console.log(c);
+     }
+     add(15,2);
+    
+    
+//    var g='global';
+// function app(){
+   
+//     console.log(g);
+    
+//     l = 'local' ;
+//     console.log(l);
+// }
+// app()
+
+
